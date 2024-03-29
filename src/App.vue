75b76eb16862198c9392451f214289cfb6fabe78
BarbashinID <80158367+BarbashinID@users.noreply.github.com>
2024-03-29 22:02:57 +0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<button v-on:click="localeRu">
+    Ru
+</button>
+<button v-on:click="localeEng">
+    Eng
+</button>
+  <div>Calendar</div>
+  <VDatePicker  v-bind:locale = language v-model="date" mode="dateTime" is-required/>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      language: 'ru',
+      date: new Date
+    }
+  },
+  methods:{
+    localeRu() {
+      this.language = 'ru'
+    },
+    localeEng() {
+      this.language = 'en'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import VCalendar from 'v-calendar';
+import 'v-calendar/style.css';
+
+createApp(App).use(VCalendar, {}).mount('#app')
